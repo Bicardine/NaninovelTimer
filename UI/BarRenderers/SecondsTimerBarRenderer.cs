43175c761ed6b4cea9114c49f44f82b150662d73
Bicardine <bicardine@gmail.com>
2024-03-29 06:55:25 +0500

    Changed: output seconds variable name

--- UI/BarRenderers/SecondsTimerBarRenderer.cs
+++ UI/BarRenderers/SecondsTimerBarRenderer.cs
@@ -11,9 +11,9 @@ namespace NaninovelTimer.UI.BarRenderers
         public override void OnTimerUpdate(float elapsedTime, float targetTime)
         {
             var remainingTimeInSeconds = NaninovelTimerUtils.GetRemaningTime(elapsedTime, targetTime);
-            var stringRemainingSeconds = ((int)remainingTimeInSeconds).ToString();
+            var rounededRemainingTimeInSeconds = ((int)remainingTimeInSeconds).ToString();
 
-            Render(stringRemainingSeconds);
+            Render(rounededRemainingTimeInSeconds);
         }
 
         public override void Render(string value)
